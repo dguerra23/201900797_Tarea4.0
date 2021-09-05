a361e5533558f735d4460c52787797d00baf0d23
dguerra23 <dguerra2300@gmail.com>
2021-09-04 20:52:12 -0600

    201900797_CobroLuz

---
+++ 201900797_CobroLuz/201900797_CobroLuz/Form1.Designer.vb
@@ -0,0 +1,451 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TxtConsumo = New System.Windows.Forms.TextBox()
+        Me.TxtSaldoAnterior = New System.Windows.Forms.TextBox()
+        Me.TxtNit = New System.Windows.Forms.TextBox()
+        Me.TxtNombre = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ComboTipoTarifa = New System.Windows.Forms.ComboBox()
+        Me.ComboTipoServicio = New System.Windows.Forms.ComboBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.ListBox8 = New System.Windows.Forms.ListBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.ListBox7 = New System.Windows.Forms.ListBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.ListBox6 = New System.Windows.Forms.ListBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.ListBox5 = New System.Windows.Forms.ListBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.LBTipoTarifa = New System.Windows.Forms.ListBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.LBTipoServicio = New System.Windows.Forms.ListBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.LBNombre = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Khaki
+        Me.GroupBox1.Controls.Add(Me.TxtConsumo)
+        Me.GroupBox1.Controls.Add(Me.TxtSaldoAnterior)
+        Me.GroupBox1.Controls.Add(Me.TxtNit)
+        Me.GroupBox1.Controls.Add(Me.TxtNombre)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(20, 20)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(533, 172)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "DATOS"
+        '
+        'TxtConsumo
+        '
+        Me.TxtConsumo.Location = New System.Drawing.Point(200, 118)
+        Me.TxtConsumo.Name = "TxtConsumo"
+        Me.TxtConsumo.Size = New System.Drawing.Size(100, 23)
+        Me.TxtConsumo.TabIndex = 7
+        '
+        'TxtSaldoAnterior
+        '
+        Me.TxtSaldoAnterior.Location = New System.Drawing.Point(200, 88)
+        Me.TxtSaldoAnterior.Name = "TxtSaldoAnterior"
+        Me.TxtSaldoAnterior.Size = New System.Drawing.Size(100, 23)
+        Me.TxtSaldoAnterior.TabIndex = 6
+        '
+        'TxtNit
+        '
+        Me.TxtNit.Location = New System.Drawing.Point(200, 58)
+        Me.TxtNit.Name = "TxtNit"
+        Me.TxtNit.Size = New System.Drawing.Size(100, 23)
+        Me.TxtNit.TabIndex = 5
+        '
+        'TxtNombre
+        '
+        Me.TxtNombre.Location = New System.Drawing.Point(200, 28)
+        Me.TxtNombre.Name = "TxtNombre"
+        Me.TxtNombre.Size = New System.Drawing.Size(100, 23)
+        Me.TxtNombre.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 126)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(126, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Consumo de Kilowatts"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Saldo Anterior"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(25, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "NIT"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(139, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre del Consumidor"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GroupBox2.Controls.Add(Me.ComboTipoTarifa)
+        Me.GroupBox2.Controls.Add(Me.ComboTipoServicio)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Location = New System.Drawing.Point(20, 220)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(533, 95)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "TIPO DE CONSUMO"
+        '
+        'ComboTipoTarifa
+        '
+        Me.ComboTipoTarifa.FormattingEnabled = True
+        Me.ComboTipoTarifa.Items.AddRange(New Object() {"Social", "No social"})
+        Me.ComboTipoTarifa.Location = New System.Drawing.Point(200, 56)
+        Me.ComboTipoTarifa.Name = "ComboTipoTarifa"
+        Me.ComboTipoTarifa.Size = New System.Drawing.Size(121, 23)
+        Me.ComboTipoTarifa.TabIndex = 3
+        '
+        'ComboTipoServicio
+        '
+        Me.ComboTipoServicio.FormattingEnabled = True
+        Me.ComboTipoServicio.Items.AddRange(New Object() {"Residencial", "Industrial"})
+        Me.ComboTipoServicio.Location = New System.Drawing.Point(200, 22)
+        Me.ComboTipoServicio.Name = "ComboTipoServicio"
+        Me.ComboTipoServicio.Size = New System.Drawing.Size(121, 23)
+        Me.ComboTipoServicio.TabIndex = 2
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(16, 64)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(76, 15)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Tipo de tarifa"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(16, 30)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(89, 15)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Tipo de servicio"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.GroupBox3.Controls.Add(Me.Label14)
+        Me.GroupBox3.Controls.Add(Me.ListBox8)
+        Me.GroupBox3.Controls.Add(Me.Label13)
+        Me.GroupBox3.Controls.Add(Me.ListBox7)
+        Me.GroupBox3.Controls.Add(Me.Label12)
+        Me.GroupBox3.Controls.Add(Me.ListBox6)
+        Me.GroupBox3.Controls.Add(Me.Label11)
+        Me.GroupBox3.Controls.Add(Me.ListBox5)
+        Me.GroupBox3.Controls.Add(Me.Label10)
+        Me.GroupBox3.Controls.Add(Me.ListBox4)
+        Me.GroupBox3.Controls.Add(Me.Label9)
+        Me.GroupBox3.Controls.Add(Me.LBTipoTarifa)
+        Me.GroupBox3.Controls.Add(Me.Label8)
+        Me.GroupBox3.Controls.Add(Me.LBTipoServicio)
+        Me.GroupBox3.Controls.Add(Me.Label7)
+        Me.GroupBox3.Controls.Add(Me.LBNombre)
+        Me.GroupBox3.Location = New System.Drawing.Point(20, 334)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(940, 183)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "TOTAL"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(806, 41)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(61, 15)
+        Me.Label14.TabIndex = 15
+        Me.Label14.Text = "Pago total"
+        '
+        'ListBox8
+        '
+        Me.ListBox8.FormattingEnabled = True
+        Me.ListBox8.ItemHeight = 15
+        Me.ListBox8.Location = New System.Drawing.Point(806, 59)
+        Me.ListBox8.Name = "ListBox8"
+        Me.ListBox8.Size = New System.Drawing.Size(106, 94)
+        Me.ListBox8.TabIndex = 14
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(694, 41)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(63, 15)
+        Me.Label13.TabIndex = 13
+        Me.Label13.Text = "Descuento"
+        '
+        'ListBox7
+        '
+        Me.ListBox7.FormattingEnabled = True
+        Me.ListBox7.ItemHeight = 15
+        Me.ListBox7.Location = New System.Drawing.Point(694, 59)
+        Me.ListBox7.Name = "ListBox7"
+        Me.ListBox7.Size = New System.Drawing.Size(106, 94)
+        Me.ListBox7.TabIndex = 12
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(582, 41)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(35, 15)
+        Me.Label12.TabIndex = 11
+        Me.Label12.Text = "Mora"
+        '
+        'ListBox6
+        '
+        Me.ListBox6.FormattingEnabled = True
+        Me.ListBox6.ItemHeight = 15
+        Me.ListBox6.Location = New System.Drawing.Point(582, 59)
+        Me.ListBox6.Name = "ListBox6"
+        Me.ListBox6.Size = New System.Drawing.Size(106, 94)
+        Me.ListBox6.TabIndex = 10
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(470, 41)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(68, 15)
+        Me.Label11.TabIndex = 9
+        Me.Label11.Text = "Pago inicial"
+        '
+        'ListBox5
+        '
+        Me.ListBox5.FormattingEnabled = True
+        Me.ListBox5.ItemHeight = 15
+        Me.ListBox5.Location = New System.Drawing.Point(470, 62)
+        Me.ListBox5.Name = "ListBox5"
+        Me.ListBox5.Size = New System.Drawing.Size(106, 94)
+        Me.ListBox5.TabIndex = 8
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(358, 41)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(82, 15)
+        Me.Label10.TabIndex = 7
+        Me.Label10.Text = "Saldo Anterior"
+        '
+        'ListBox4
+        '
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.ItemHeight = 15
+        Me.ListBox4.Location = New System.Drawing.Point(358, 62)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.Size = New System.Drawing.Size(106, 94)
+        Me.ListBox4.TabIndex = 6
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(246, 41)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(76, 15)
+        Me.Label9.TabIndex = 5
+        Me.Label9.Text = "Tipo de tarifa"
+        '
+        'LBTipoTarifa
+        '
+        Me.LBTipoTarifa.FormattingEnabled = True
+        Me.LBTipoTarifa.ItemHeight = 15
+        Me.LBTipoTarifa.Location = New System.Drawing.Point(246, 62)
+        Me.LBTipoTarifa.Name = "LBTipoTarifa"
+        Me.LBTipoTarifa.Size = New System.Drawing.Size(106, 94)
+        Me.LBTipoTarifa.TabIndex = 4
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(134, 41)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(89, 15)
+        Me.Label8.TabIndex = 3
+        Me.Label8.Text = "Tipo de servicio"
+        '
+        'LBTipoServicio
+        '
+        Me.LBTipoServicio.FormattingEnabled = True
+        Me.LBTipoServicio.ItemHeight = 15
+        Me.LBTipoServicio.Location = New System.Drawing.Point(134, 62)
+        Me.LBTipoServicio.Name = "LBTipoServicio"
+        Me.LBTipoServicio.Size = New System.Drawing.Size(106, 94)
+        Me.LBTipoServicio.TabIndex = 2
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(22, 41)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(51, 15)
+        Me.Label7.TabIndex = 1
+        Me.Label7.Text = "Nombre"
+        '
+        'LBNombre
+        '
+        Me.LBNombre.FormattingEnabled = True
+        Me.LBNombre.ItemHeight = 15
+        Me.LBNombre.Location = New System.Drawing.Point(22, 62)
+        Me.LBNombre.Name = "LBNombre"
+        Me.LBNombre.Size = New System.Drawing.Size(106, 94)
+        Me.LBNombre.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(633, 220)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(633, 250)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Limpiar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(633, 280)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Salir"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(972, 572)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TxtConsumo As TextBox
+    Friend WithEvents TxtSaldoAnterior As TextBox
+    Friend WithEvents TxtNit As TextBox
+    Friend WithEvents TxtNombre As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ComboTipoTarifa As ComboBox
+    Friend WithEvents ComboTipoServicio As ComboBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label14 As Label
+    Friend WithEvents ListBox8 As ListBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents ListBox7 As ListBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents ListBox6 As ListBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents ListBox5 As ListBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents ListBox4 As ListBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents LBTipoTarifa As ListBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents LBTipoServicio As ListBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents LBNombre As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ 201900797_CobroLuz/201900797_CobroLuz/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Public Const Residencial1 As Double = 0.99
+    Public Const Residencial2 As Double = 1.2
+    Public Const Residencial3 As Double = 3.0
+
+    Public Const Industrial1 As Double = 1.99
+    Public Const Industrial2 As Double = 2.2
+    Public Const Industrial3 As Double = 4.0
+
+    Public indice As Byte = 0
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+End Class
